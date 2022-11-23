243fcee050ef02b939ee7b195343918ddedf6c59
Iogan <ivan_gulyaev@yahoo.com>
2022-11-23 20:46:41 +0700

    Program completed, add ScreenShot_Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,7 @@
+﻿int userNumber = new int();
+Console.Write("Enter number: ");
+userNumber = Convert.ToInt32(Console.ReadLine());
+if (userNumber % 2 == 0)
+    Console.Write("да");
+else
+    Console.Write ("Нет");
