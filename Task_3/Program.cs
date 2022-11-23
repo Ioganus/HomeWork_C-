7abe5c05283ddc2752bbde090d90510dc673dd94
Iogan <ivan_gulyaev@yahoo.com>
2022-11-23 21:16:40 +0700

    Program completed, add ScreenShot_Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿int N = new int();
+Console.Write ("Enter number: ");
+N = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+Console.WriteLine("Even numbers");
+while(count<N-1)
+{
+    count += 2;
+    Console.WriteLine(count);
+}
